de16dfed7b607f63aa00011aa46b3baff0240fde
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-22 14:11:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,9 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for]= "GUI"
+programmer_hash
+
 end
 
 def adding_to_dennis
@@ -131,4 +134,6 @@ def adding_to_dennis
       }
     }
 
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
